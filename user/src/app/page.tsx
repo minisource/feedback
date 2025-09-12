5d952bc43122ec18cf3fdb3edcae3cd237092591
ebrahim <ebrahimbrooz@gmail.com>
2025-09-12 03:37:48 +0330

    remove pages folder

---
+++ user/src/app/page.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App!</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+}
