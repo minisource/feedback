2661d8ee7f509fe5071ca304ab1fc53350c950e6
ebrahim <ebrahimbrooz@gmail.com>
2025-09-17 11:51:56 +0330

    update

--- user/src/app/page.tsx
+++ user/src/app/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function HomePage() {
   return (
     <div>
-      <h1>this is test devops</h1>
+      <h1>this is test</h1>
       <p>This is the home page.</p>
     </div>
   );
